baca12dcd8b20cf9a6b1138f0caf0360fb94ac48
fermza <45858786+fermza@users.noreply.github.com>
2025-07-01 16:13:29 -0300

    new functionalities
    Added: 
    - auto calculation of -l based on number of sequences in fasta file and length of code
    - naming of output files with flag --new_filename

--- seq_renamer.py
+++ seq_renamer.py
@@ -22,6 +22,7 @@ parser.add_argument('-ext', default='*.fa', help='Extension of files if multiple
 parser.add_argument('-id', default='', help='ID Key sequence name')
 parser.add_argument('-taxid', default='', help='Tax ID (species level)')
 parser.add_argument('-l', default=10, help='Seq name string lenght.')
+parser.add_argument('--new_filename', default='', help='Enter a string or tsv column name to name output files.')
 
 args = vars(parser.parse_args())
 
@@ -31,6 +32,8 @@ ext = str(args['ext'])
 key = str(args['id'])
 taxid = str(args['taxid'])
 str_len = int(args['l'])
+output_filename = str(args['new_filename'])
+orig_str_len = int(args['l'])  # Store the original argument value
 ###############################################################################
 
 
@@ -48,6 +51,7 @@ def input_files(ftype):
 def seqs_extractor(file_fasta):
     names = []
     seqs = []
+    seq_num = []
     for seq_record in SeqIO.parse(file_fasta, "fasta"):
         names.append(seq_record.description)
         seqs.append(seq_record.seq)
@@ -61,6 +65,17 @@ if tsv_file == '':
         exit()
     names, seqs = seqs_extractor(fasta_file)
 
+    max_seq = len(names)
+    if str_len == 0:
+        # Auto-adjust str_len
+        str_len = len(key) + len(str(max_seq))
+        print(f"Auto-adjusted -l to {str_len} to fit {max_seq} sequences with key '{key}'.")
+
+    nk = str_len - len(key)
+    if max_seq > 10**nk - 1:
+        print(f"Error: -l value ({str_len}) is too small for {max_seq} sequences with key '{key}'. Increase -l.")
+        exit(1)
+
     new_names = []
 
     for name, l in zip(names, list(range(1, len(names) + 1))):
@@ -68,12 +83,19 @@ if tsv_file == '':
         nn = str(key) + str(l).zfill(nk)
         new_names.append(nn)
 
-    with open(str(real_fasta) + "_renamed.fsa", "w") as f1:
+    if output_filename:
+        # If a new filename is provided, use it for the output files
+        out_file = output_filename
+    else:
+        # Otherwise, use the original fasta file name
+        out_file = real_fasta
+    
+    with open(str(out_file) + "_rnmd.fsa", "w") as f1:
         for i, j in zip(new_names, seqs):
             f1.write(">" + str(i) + "\n")
             f1.write(str(j) + "\n")
 
-    with open(str(real_fasta) + "_file_map", "w") as f2:
+    with open(str(out_file) + "_file_map", "w") as f2:
         for i, j in zip(new_names, names):
             print(i, '\t', j, '\t', taxid, file=f2)
 
@@ -108,22 +130,47 @@ else:
             key = row['code']
             taxid = row['taxid']
 
-            # Process only if the file name from DataFrame matches the current fasta file being processed
+            # Determine output file name
+            if output_filename:
+                if output_filename not in fasta_code_df.columns:
+                    print(f"Error: The column '{output_filename}' does not exist in the TSV file. Please provide a valid column name with --new_filename.")
+                    exit(1)
+                out_file = str(row[output_filename])
+                if not out_file or pd.isna(out_file):
+                    print(f"Error: The column '{output_filename}' is empty for file '{fasta_file_path}'. Please provide a value in the TSV.")
+                    exit(1)
+            else:
+                out_file = real_fasta
+
             if fasta == fasta_file_path:
                 names, seqs = seqs_extractor(fasta_file_path)
 
+                max_seq = len(names)
+                # Use a local str_len for each file
+                if orig_str_len == 0:
+                    str_len = len(key) + len(str(max_seq))
+                    print(f"Auto-adjusted -l to {str_len} to fit {max_seq} sequences with key '{key}' in file '{fasta_file_path}'.")
+                else:
+                    str_len = orig_str_len
+
+                nk = str_len - len(key)
+                if max_seq > 10**nk - 1:
+                    print(f"Error: -l value ({str_len}) is too small for {max_seq} sequences with key '{key}' in file '{fasta_file_path}'. Increase -l.")
+                    exit(1)
+
                 new_names = []
+
                 for name, l in zip(names, list(range(1, len(names) + 1))):
                     nk = str_len - len(key)
                     nn = str(key) + str(l).zfill(nk)
                     new_names.append(nn)
 
-                with open(f"{real_fasta}_renamed.fsa", "w") as f1:
+                with open(f"{out_file}_rnmd.fsa", "w") as f1:
                     for i, j in zip(new_names, seqs):
                         f1.write(">" + str(i) + "\n")
                         f1.write(str(j) + "\n")
 
-                with open(f"{real_fasta}_file_map", "w") as f2:
+                with open(f"{out_file}_file_map", "w") as f2:
                     for i, j in zip(new_names, names):
                         f2.write(f"{i}\t{j}\t{taxid}\n")
 
@@ -131,5 +178,3 @@ else:
                 break  # Exit the inner loop once the matching file is processed
 
     print(f"Execution Successful: {datetime.now() - startTime}")
-
-    print(("Execution Successful: " + str((datetime.now() - startTime))))
